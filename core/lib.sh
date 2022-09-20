0c70cc0dca76d2022116006539a1c44c03d4a0e7
Esteban Garviso <e.garvisovenegas@gmail.com>
2022-09-20 11:05:10 -0300

    fix core scripts to be import with wget

--- core/lib.sh
+++ core/lib.sh
@@ -1,10 +1,13 @@
 [[ -z $CORE_SOURCE ]] && {
     _coreDir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
     # import trap handler
-    source "$_coreDir/lib.trap.sh"
+    # source "$_coreDir/lib.trap.sh"
+    wget -qO- https://raw.githubusercontent.com/estebangarviso/bash-scripts/master/core/lib.trap.sh | bash
     # import variables
-    source "$_coreDir/_variables.sh"
+    # source "$_coreDir/_variables.sh"
+    wget -qO- https://raw.githubusercontent.com/estebangarviso/bash-scripts/master/core/_variables.sh | bash
     # import functions
-    source "$_coreDir/_helpers.sh"
+    # source "$_coreDir/_helpers.sh"
+    wget -qO- https://raw.githubusercontent.com/estebangarviso/bash-scripts/master/core/_helpers.sh | bash
     CORE_SOURCE="${BASH_SOURCE[0]}"
 }
